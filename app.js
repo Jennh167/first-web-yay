a654634b48739c7c7793b36e4bb7b2dd085a981b
Jennifer Hoskinson <jennh167@gmail.com>
2023-10-01 14:00:08 -0700

    adding lab 7

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 'use strict';
 
-console.log("Peak a Boo");
-alert("Welcome Home!");
 
-const username = prompt("What can I call you?");
-console.log(username);
-
-document.write("Hello " + username + "! So glad you're here!"); 
-
-if (username == "Kassie"){
-    document.write(" You're AMAZING Teach!!");
-} else if(username == "Joey"){
-    document.write(" Couldn't do it without you!");
-}else if(username == "Ben"){
-    document.write(" Couldn't do it without you!");
-} else if(username == "Brandon"){
-    document.write(" Couldn't do it without you!");
-} else {
-    document.write(" The world is a better place with you in it!");
+function getName(){
+ const userName = prompt("What can I call you?");
+ console.log (userName);
+ return userName;
 }
-var color = prompt("What color background would you like?");
-document.bgColor = color;
+
+function greetUser(){
+ document.write("Hello " + theirName + "! So glad you're here!"); 
+}
+
+function specialGreet(userName){
+    console.log("userName: " + userName);
+    if (userName == "" || userName == null){
+        userName = prompt("Please??");
+    }
+
+    if (userName == "" || userName == null){
+    userName = prompt("Pretty Please??"); 
+    }
+    if (userName == "Kassie"){
+    document.write(" You're AMAZING Teach!!");
+    } else if(userName == "Joey"){
+    document.write(" Couldn't do it without you!");
+    } else if(userName == "Ben"){
+    document.write(" Couldn't do it without you!");
+    } else if(userName == "Brandon"){
+    document.write(" Couldn't do it without you!");
+    } else {
+    document.write(" The world is a better place with you in it!");
+    }
+}
+function colorPicker(){
+    var color = prompt("What color background would you like?");
+    document.bgColor = color;
+}
